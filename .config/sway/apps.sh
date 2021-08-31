25a5957064327ed4425e7404e33a7bee202d179e
dahead <dahead@mailbox.org>
2021-08-31 14:33:57 +0200

    Add files via upload

---
+++ .config/sway/apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# switch video to auxiliary device
+# ./video_home.sh
+
+# start applications...
+(kitty &)
+(pcmanfm &)
+(firefox &)
+# (chromium &)
+# (thunderbird &)
+(pavucontrol &)
---
+++ .config/sway/screen.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Show sway display output modes
+# swaymsg -t get_outputs
+
+# Integrated Display
+SCR_MAIN="eDP-1"
+
+# External monitor list
+SCR_AUX1="DP-1"
+SCR_AUX2="DP-2"
+SCR_AUX3="DP-3"
+SCR_AUX4="DP-4"
+SCR_AUX5="DP-5"
+SCR_AUX6="DP-6"
+SCR_AUX8="DP-8"
+
+# resolution list
+RES_STD="1920x1050"
+RES_MID="1920x1080"
+RES_MAX="2560x1440"
+
+# Get output of swaymsg -t get_outputs
+#OUTPUT=$(swaymsg -t get_outputs)
+#if [[ -n "$OUTPUT" ]]; then
+
+# Use HDMI with LG
+# HiDPI Mode
+swaymsg output $SCR_AUX3 scale 1
+swaymsg output $SCR_AUX3 enable
+swaymsg output $SCR_AUX3 res $RES_MID
+
+# shutdown laptop display
+swaymsg output $SCR_MAIN disable
+
+	# swaymsg output $LAPTOP disable
+#fi
