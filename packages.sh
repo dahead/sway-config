94c89535d4e1f492fc300dd138667c729b0997cd
dahead <dahead@mailbox.org>
2022-04-13 18:36:12 +0200

    Add files via upload

--- packages.sh
+++ packages.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 # Base system
-sudo pacman -S --noconfirm efibootmgr iwd mkinitcpio mkinitcpio-busybox curl wget 
+sudo pacman -S --noconfirm efibootmgr iwd mkinitcpio curl wget
+# sudo pacman -S --noconfirm mkinitcpio-busybox 
 # Obsolete since iwd 0.19
 # sudo pacman -S --noconfirm dhcpcd
 
+# shell
+sudo pacman -R --noconfirm zsh zsh-completions zsh-syntax-highlighting zsh-theme-powerlevel10k
+
 # desktop, window manager >>> sway
 sudo pacman -S --noconfirm wayland wayland-protocols sway swaylock swaybg swayidle i3status mako bemenu bemenu-wayland xorg-xwayland lxappearance nitrogen
+# sudo pacman -R --noconfirm archlinux-wallpaper 
+
+# login manager
 # sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings 
 
-# System Tools
-sudo pacman -R --noconfirm sudo nano neovim kitty kitty-shell-integration kitty-terminfo pcmanfm ffmpegthumbnailer file-roller geany
+# System tools
+sudo pacman -R --noconfirm sudo kitty kitty-shell-integration kitty-terminfo 
 sudo pacman -R --noconfirm awesome-terminal-fonts ttf-font-awesome
-# sudo pacman -R --noconfirm archlinux-wallpaper 
 sudo pacman -R --noconfirm htop tree remmina rsync putty p7zip  
 sudo pacman -R --noconfirm pulseaudio pavucontrol keepassxc
 # Obsolete?
-# sudo pacman -R --noconfirm dialog wpa_supplicant alsa-utils  inetutils
+# sudo pacman -R --noconfirm dialog wpa_supplicant alsa-utils inetutils
+
+# file manager
+sudo pacman -R --noconfirm pcmanfm ffmpegthumbnailer file-roller
+
+# Text editor, terminal
+sudo pacman -R --noconfirm nano neovim geany
 
 # Security
 # sudo pacman -R --noconfirm ufw rkhunter nmap
@@ -24,24 +36,21 @@ sudo pacman -R --noconfirm pulseaudio pavucontrol keepassxc
 # Internet
 sudo pacman -R --noconfirm firefox youtube-dl 
 
-# Media Tools
-sudo pacman -R --noconfirm mpv grim viewnior shotcut cmus
+# Media
+sudo pacman -R --noconfirm mpv grim viewnior cmus
 sudo pacman -R --noconfirm gst-plugins-base
 # Big packages...
-# sudo pacman -R --noconfirm gimp
-
-# shell
-sudo pacman -R --noconfirm zsh zsh-completions zsh-syntax-highlighting zsh-theme-powerlevel10k
+# sudo pacman -R --noconfirm gimp shotcut
 
 # Development
-sudo pacman -R --noconfirm visual-studio-code-bin qemu git virt-manager
+# sudo pacman -R --noconfirm visual-studio-code-bin qemu git virt-manager
 
 # Lenovo X1C9 specific
 sudo pacman -R --noconfirm xf86-video-intel xf86-video-nouveau lm_sensors fwupd brightnessctl-logind 
 # sudo pacman -R --noconfirm xf86-video-amdgpu sof-firmware
 
 # Gaming
-# sudo pacman -R --noconfirm steam
+# sudo pacman -R --noconfirm steam dosbox wine
 
 # Old xorg stuff
 # sudo pacman -R --noconfirm xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xkbcomp xorg-xprop xorg-xrandr xorg-xset xorg-xwayland 
